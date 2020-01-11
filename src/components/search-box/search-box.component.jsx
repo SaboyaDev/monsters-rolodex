7ae7b615d3c9b610728bea3f49882006bdd94aa0
Jose L Saboya Rosales <joselsaboyarosales@Joses-MacBook-Pro.local>
2020-01-11 16:39:41 -0500

    Created a resuable search component that is dynamic

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, handlechange }) => (
+  <input type='search' placeholder={placeholder} onChange={handlechange} />
+);
