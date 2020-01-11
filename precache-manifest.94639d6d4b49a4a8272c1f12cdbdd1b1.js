8239be8e33e713948207d6eddbf39fa6dc2d045e
Jose L Saboya Rosales <joselsaboyarosales@Joses-MacBook-Pro.local>
2020-01-11 17:53:11 -0500

    Updates

---
+++ precache-manifest.94639d6d4b49a4a8272c1f12cdbdd1b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c30c8dca4f3c24e40accc835d9db45",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f045c0e780f779d17808",
+    "url": "/monsters-rolodex/static/css/main.7ca64104.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f045c0e780f779d17808",
+    "url": "/monsters-rolodex/static/js/main.e356aed0.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
